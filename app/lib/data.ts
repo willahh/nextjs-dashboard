8d32f765692f6784ca8a5b2a780a725f75846b50
William Ravel <wravel@gmail.com>
2024-11-18 10:17:14 +0100

    End of chapter 8

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,10 +12,9 @@ import { formatCurrency } from './utils';
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Data fetch completed after 3 seconds');
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
